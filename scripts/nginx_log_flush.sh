3c9dc5326f2673d2a62c9ff2aef66c3363e26cbb
unknown <csmcc2@hotmail.com>
2026-05-07 10:30:52 +0800

    update

--- scripts/nginx_log_flush.sh
+++ scripts/nginx_log_flush.sh
@@ -2,12 +2,16 @@
 
 LOGS_PATH="/var/log/nginx"
 ACCESS_LOG="${LOGS_PATH}/access.log"
+ARCHIVE_DIR="${LOGS_PATH}/archive_access"
+
+mkdir -p "$ARCHIVE_DIR"
 
 if [[ -f "$ACCESS_LOG" ]]; then
 	yesterday=$(date +"%F" -d "-1 days")
-	mv "$ACCESS_LOG" "${LOGS_PATH}/access-${yesterday}.log"
-	gzip "${LOGS_PATH}/access-${yesterday}.log"
+	ARCHIVE_FILE="${ARCHIVE_DIR}/access-${yesterday}.log.gz"
+	gzip -c "$ACCESS_LOG" >"$ARCHIVE_FILE"
+	: >"$ACCESS_LOG"
 	/usr/local/nginx/sbin/nginx -s reload
 fi
 
-find "$LOGS_PATH" -name "access-*.log.gz" -mtime +30 -delete
+find "$ARCHIVE_DIR" -name "access-*.log.gz" -mtime +30 -delete
